5b2a14e6787ca5b56aa859746699736f26da2e82
zeroCool-1988 <adoniyasshimelis110@gmail.com>
2026-07-09 17:44:38 +0300

    fix: increased the connection timeout for Render

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 30000,
   ssl: {
     rejectUnauthorized: false
   }
